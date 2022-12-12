38343fc9576f079bfcc94b5a0a7dd471bf8dd2a9
Gubbalanagasai <119146937+Gubbalanagasai@users.noreply.github.com>
2022-12-12 20:43:32 +0530

    Add files via upload

---
+++ python week-2/class12.py
@@ -0,0 +1,6 @@
+a = 5
+print("value of a is %d" % (a))
+
+name = "Sai Kiran"
+company = "india"
+print("name = {name} company = {company}".format(name = name , company = company))
---
+++ python week-2/class17.py
@@ -0,0 +1,14 @@
+#object oriented programme
+student1 = {"name":"sai kiran","marks":50}
+student2 = {"name":"jaswanth","marks":40}
+print()
+
+class Person:
+    pass
+
+p = Person()
+print(p)
+
+a =1 
+def square(a):
+    print(a**2)
